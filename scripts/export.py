242be4acd7d796133155f05449f42bb3b9a38f54
Michael Cuffaro <consulting@michaelcuffaro.com>
2023-01-10 11:28:01 -0500

    use uri=True when connecting to sqlite db

--- scripts/export.py
+++ scripts/export.py
@@ -393,10 +393,8 @@ if __name__ == "__main__":
                 print(f"The database '{path}' does not exist.", file=sys.stderr)
                 sys.exit(1)
             params = m[4] or ""
-            if params:
-                print(f"Warning: Ignoring parameter string '{params}'")
-            db = f"{path}"
-            with sqlite3.connect(db) as conn:
+            db = f"file:{path}{params}"
+            with sqlite3.connect(db, uri=True) as conn:
                 cursor = conn.cursor()
                 func(cursor, True, args)
         else:
